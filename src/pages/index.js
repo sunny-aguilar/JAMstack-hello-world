fb924ee6003eb71c82794425c00ac9902b03629f
Sunny <sunny.aguilar@gmail.com>
2018-08-29 17:40:40 -0700

    update link

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hello World!</h1>
     <p>My name is Sunny.</p>
     <p>I'm currently studying computer science at OSU. Web development has alway been a hobby of mine and I'm taking it to the next level =D.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Next Page</Link>
   </div>
 )
 
