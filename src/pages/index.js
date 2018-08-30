3a01cf8089f8a7a005ce5e9bd05b9e170319b043
Sunny <sunny.aguilar@gmail.com>
2018-08-29 18:20:29 -0700

    update page with FCC

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
     <p>My name is Sunny.</p>
     <p>I'm currently studying computer science at OSU. Web development has alway been a hobby of mine and I'm taking it to the next level =D.</p>
     <p>My current studies have me focused on C++ and Python which has helped my understanding of web technologies and computers overall.</p>
+    <p>I'm also currently working on FCC's front-end certificate when I can get a break from school since it can be overwhelming!</p>
     <Link to="/page-2/">Next Page</Link>
   </div>
 )
